9e21de838172e55d5c9ab798f71d05b73fe18818
Sanuda Rusara <sanudarusara@gmail.com>
2026-04-10 19:04:03 +0530

    feat: implemented the backend for the payment-service

---
+++ services/payment-service/src/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI); // options removed
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+  }
+};
+
+module.exports = connectDB;
